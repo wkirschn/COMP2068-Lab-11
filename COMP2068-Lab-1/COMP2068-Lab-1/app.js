9d008ab4d39b9329e9c3bfec6df84db2d094f13e
Wyatt <wkirschn@uoguelph.ca>
2020-09-25 12:34:36 -0400

    1.3 Creating another change after pulling down app.js from GitHub

--- COMP2068-Lab-1/COMP2068-Lab-1/app.js
+++ COMP2068-Lab-1/COMP2068-Lab-1/app.js
@@ -2,3 +2,4 @@
 
 console.log('Hello world');
 console.log('I am making a change inside of GitHub');
+console.log('After pulling this down to my computer, I am making one more change to this file.');
